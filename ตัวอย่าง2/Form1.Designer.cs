85efd9693124b6b06a8ef52eb2c915be2ef6f6e9
panupongkongkate <61161511+panupongkongkate@users.noreply.github.com>
2020-04-19 13:12:10 +0700

    image-enhancement-with-emgu.git

--- ตัวอย่าง2/Form1.Designer.cs
+++ ตัวอย่าง2/Form1.Designer.cs
@@ -29,12 +29,244 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(32, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(510, 309);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Location = new System.Drawing.Point(582, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(516, 309);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 3;
+            this.imageBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 62);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(236, 62);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(582, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(236, 62);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "แปลงภาพเทา";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(862, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 62);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Resize";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(32, 459);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(236, 62);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Brightness";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(306, 459);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(236, 62);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "crop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(582, 459);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(236, 62);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "ROTATION";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(862, 459);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(236, 62);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "SMOOTH FILTER";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "hight";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(598, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(859, 337);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "hight";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 334);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(320, 334);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(651, 334);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(900, 337);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1149, 552);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- ตัวอย่าง2/Form1.cs
+++ ตัวอย่าง2/Form1.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
 
 namespace ตัวอย่าง2
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog op = new OpenFileDialog(); //อย่างลืมเรียกclass มาใช้นะ
+        SaveFileDialog sa = new SaveFileDialog();
+        Image<Rgb, byte> imagecome;
+        Image<Rgb, byte> imageprocess ;
         public Form1()
         {
             InitializeComponent();
+            op.Filter = "JPEG file(*.JPG|*.Jpg|All file(*.*)|*.*)";
+            sa.Filter = "JPEG file(*.JPG|*.Jpg|All file(*.*)|*.*)";
+        }
+
+        private void button1_Click(object sender, EventArgs e) //รับภาพ
+        {
+            if (op.ShowDialog() == DialogResult.OK) //op.showdialog เป็นการบอกว่า ถ้ามีการกด เปิดไดอะล็อก ให้ เข้ามาในนี้
+            {
+                imagecome = new Image<Rgb, byte>(op.FileName);
+                imageBox1.Image = imagecome;
+                textBox1.Text = imagecome.Width.ToString();
+                textBox2.Text = imagecome.Height.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //โหลดภาพ
+        {
+            if (imagecome != null)
+            {
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    imageprocess = imagecome.Convert<Rgb, byte>();
+                    imageprocess.Save(sa.FileName); //save file
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //แปลงภาพเทา
+        {
+            if (imagecome != null)
+            {
+                imageprocess = imagecome.Convert<Rgb, byte>();
+                Image<Gray, byte> G1 = imageprocess.Convert<Gray, byte>();
+                imageBox2.Image = G1;
+                textBox3.Text = G1.Width.ToString();
+                textBox4.Text = G1.Height.ToString();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //ปรับขนาดภาพ
+        {
+            if (imagecome != null)
+            {
+                imageprocess = imagecome.Convert<Rgb, byte>(); //รับค่าภาพ
+                                                               // Image<Gray, byte> G1 = imageprocess.Convert<Gray,byte>();
+                Image<Rgb, Byte> image1 = imageprocess.Resize(100, 100, Emgu.CV.CvEnum.Inter.Nearest);
+                imageBox2.Image = image1;
+                textBox3.Text = image1.Width.ToString();
+                textBox4.Text = image1.Height.ToString();
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) //ปรับแสงให้ ใสขึ้น
+        {
+            if(imagecome != null)
+            {
+                imageprocess = imagecome.Mul(1.5);
+                imageBox2.Image = imageprocess;
+                textBox3.Text = imageprocess.Width.ToString();
+                textBox4.Text = imageprocess.Height.ToString();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if(imagecome != null)
+            {
+                imageprocess = Crop(imagecome, new Point(200, 200), new Size(500, 500));
+                //input1= ภาพที่ต้องการ,input2 = จุดเริ่มต้น , input3 = ขนาด ที่ต้องการ
+                imageBox2.Image = imageprocess;
+                textBox3.Text = imageprocess.Width.ToString();
+                textBox4.Text = imageprocess.Height.ToString();
+            }
+        }
+        public static Image<Rgb,byte>Crop(Image<Rgb,byte>image0,Point start ,Size s) //ฟังชั้นการตัด 
+        {
+
+            image0.ROI = new Rectangle(start,s);//ตีพน4เหลี่ยม
+            Image<Rgb, byte> image1 = image0.Copy();
+            return image1;
+        }
+
+        private void button7_Click(object sender, EventArgs e) // การหมุนภาพ 
+        {
+            if(imagecome != null)
+            {
+                imageprocess = imagecome.Rotate(45, new Rgb(0, 0, 0), true); 
+                //inputแรกคือการหมุน ,input2 คือการปรับสีพื้นหลัง , input 3 คือการ ตัด True =ตัด false = ไม่ตัด 
+                imageBox2.Image = imageprocess ;
+                textBox3.Text = imageprocess.Width.ToString();
+                textBox4.Text = imageprocess.Height.ToString();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if(imagecome != null)
+            {
+                imageprocess = imagecome.SmoothBlur(50, 50);//เรียกใช้ฟังชั้นเบอ
+                imageBox2.Image = imageprocess;
+                textBox3.Text = imageprocess.Width.ToString();
+                textBox4.Text = imageprocess.Height.ToString();
+            }
         }
     }
 }
